eefdb0f144963ce76d2560d1d170592cbd8ba767
Jose-Contreras <104838758+JoseContrerasP@users.noreply.github.com>
2023-11-17 11:30:45 -0400

    Comment if-statement

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-	echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser($SUPERUSER_USERNAME, $SUPERUSER_EMAIL, $SUPERUSER_PASSWORD)" | python manage.py shell
-fi
+# if [[ $CREATE_SUPERUSER ]];
+# then
+# 	echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser($SUPERUSER_USERNAME, $SUPERUSER_EMAIL, $SUPERUSER_PASSWORD)" | python manage.py shell
+# fi
