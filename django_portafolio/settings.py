461c468a5b52806b499ab00e33723b8bf471483a
Jose-Contreras <104838758+JoseContrerasP@users.noreply.github.com>
2023-11-17 16:09:45 -0400

    add media_root to staticfiles_dirs 4

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
         BASE_DIR,
         "static",
     ),
-    os.path.join(BASE_DIR, "media"),
+    os.path.join(BASE_DIR, "/opt/render/project/src/media"),
 ]
 
 # Default primary key field type
